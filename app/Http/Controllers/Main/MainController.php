614d9a421233d2137263ab0fed50569b5d4bb65d
unknown <vohungnghiem@gmail.com>
2021-11-09 11:07:40 +0700

    upload home

--- app/Http/Controllers/Main/MainController.php
+++ app/Http/Controllers/Main/MainController.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\DB;
 class MainController extends Controller
 {
     public function home() {
-        return view('main.home');
+        $banner = DB::table('vhn_banners')->where('stt', '=', '1')->orderBy('id','asc')->get();
+        $gioithieuhome = DB::table('vhn_gioithieuhome')->where([['stt', '=', '1'],['ten','!=','ACM']])->orderBy('id','asc')->get();
+        $gioithieuhomefirst= DB::table('vhn_gioithieuhome')->where([['stt', '=', '1'],['ten','=','ACM']])->orderBy('id','asc')->get();
+        $muctieu = DB::table('vhn_muctieuhome')->orderBy('id','desc')->get();
+        $hosocanchuanbi = DB::table('vhn_hosocanchuanbi')->where('stt', '=', '1')->orderBy('id','asc')->get();
+        $camnhanhocvien = DB::table('vhn_camnhanhocvien')->where('stt', '=', '1')->orderBy('id','asc')->get();
+        $tintuc = DB::table('vhn_tintucs')->where([['stt', '=', '1'],['id_loaitintuc','=','1']])->take(6)->orderBy('id','desc')->get();
+        $tintucorther = DB::table('vhn_tintucs')->where([['stt', '=', '1'],['id_loaitintuc','=','1']])->skip(6)->take(6)->orderBy('id','desc')->get();
+        $truonghoc = DB::table('vhn_truonghoc')->where('stt', '=', '1')->orderBy('id','desc')->get();
+        $hinhanh = DB::table('vhn_hinhanhs')->where('stt','=','1')->orderBy('id','desc')->get();
+        return view('main.home',compact('banner','gioithieuhome','gioithieuhomefirst','muctieu','hosocanchuanbi','camnhanhocvien','tintuc','tintucorther','truonghoc','hinhanh'));
     }
 }
--- resources/views/main/layouts/menu.blade.php
+++ resources/views/main/layouts/menu.blade.php
@@ -1,3 +1,18 @@
+@php
+    $menu = DB::table('vhn_menu')->where('stt', '=', '1')->orderBy('id','asc')->get();
+    function showmenu($item) {
+        $showmenu = '';
+        if (App::getLocale() == 'vi') {
+            $showmenu = $item->ten;
+        }else{
+            $os = array(3,4);
+            if (!in_array($item->id, $os)) {
+                $showmenu = $item->tenjp;
+            }
+        }
+        return $showmenu;
+    }
+@endphp
 <header>
     <div class="top-header">
         <div class="container">
@@ -6,10 +21,11 @@
                     <div class="logo"><a href="#"><img src="main_template/img/logo.png" alt="logo"></a></div>
                     <div class="hamburger"></div>
                     <nav class="header-nav">
-                        <li><a class="active" href="index.html">trang chủ</a></li>
-                        <li><a href="don-hang.html">đơn hàng</a></li>
-                        <li><a href="dieu-duong-2.html">Điều dưỡng</a></li>
-                        <li><a href="tin-tuc.html">tin tức</a></li>
+                        <li><a class="active" href="/">@lang('main.trangchu')</a></li>
+						<li><a href="/gioithieu">@lang('main.gioithieu')</a></li>
+                        @foreach ($menu as $item)
+							<li><a href="{{$item->link}}">{{showmenu($item)}}</a></li>
+						@endforeach
                         @foreach (Config::get('languages') as $lang => $language)
                             @if ($language['status'] == 1)
                                 <li><a href="{{ route('lang.switch', $lang) }}"><img src="main_template/img/flag{{ $language['flag-icon'] }}.png" alt="flag{{ $language['flag-icon'] }}.png"></a></li>
@@ -30,10 +46,9 @@
                     @endif
                 @endforeach
             </li>
-            <li><a class="active" href="index.html">trang chủ</a></li>
-            <li><a href="don-hang.html">đơn hàng</a></li>
-            <li><a href="dieu-duong-2.html">Điều dưỡng</a></li>
-            <li><a href="tin-tuc.html">tin tức</a></li>
+            @foreach ($menu as $item)
+                <li><a href="{{$item->link}}">{{showmenu($item)}}</a></li>
+            @endforeach
         </nav>
     </div>
 </header>
